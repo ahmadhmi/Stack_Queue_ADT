a0c412d986cb6b449932b0f7a6fb8d117904750d
Ahmad Heshmati <amd.heshmati@gmail.com>
2023-11-22 18:53:32 -0700

    fixed the import errors

---
+++ src/main/java/org/example/EmptyQueueException.java
@@ -0,0 +1,5 @@
+package org.example;
+
+
+public class EmptyQueueException extends Exception {
+}
--- src/main/java/org/example/MyQueue.java
+++ src/main/java/org/example/MyQueue.java
@@ -1,4 +1,58 @@
 package org.example;
 
-public class MyQueue implements Stack{
+public class MyQueue implements QueueADT{
+    @Override
+    public void enqueue(Object toAdd) throws NullPointerException {
+
+    }
+
+    @Override
+    public Object dequeue() throws EmptyQueueException {
+        return null;
+    }
+
+    @Override
+    public Object peek() throws EmptyQueueException {
+        return null;
+    }
+
+    @Override
+    public void dequeueAll() {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public boolean equals(QueueADT that) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public Object[] toArray(Object[] holder) throws NullPointerException {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
